52c9c6b0a50d917e63a917594e7e11d8a650510a
MichaelLugo313 <michaellugo@gmail.com>
2025-07-16 19:14:41 -0500

    Assignment complete

---
+++ array-iterator-methods.js
@@ -0,0 +1,20 @@
+// Task 1
+let cityList = ["St. Louis", "Chicago", "Amsterdam", "Tokyo", "Copenhagen"];
+cityList.forEach(city => {
+    console.log(city.toUpperCase);
+}) // Why doesn't this work?
+
+// Task 2
+let numbers = [1, 2, 3, 4, 5];
+let squares = numbers.map(num => num ** 2);
+console.log(squares); // 1, 4, 9, 16, 25
+
+// Task 3
+let scores = [85, 42, 90, 75, 30, 100];
+let highScores = scores.filter(score => score >= 80);
+console.log(highScores); // 85, 90, 100
+
+// Task 4
+let favoriteFood = ["Steak", "Spaghetti", "Creme Brulee", "Spinach", "Pizza"];
+let foundFood = favoriteFood.find(food => food.length > 4);
+let foundFoodIndex = favoriteFood.findIndex(food => food === foundFood);
